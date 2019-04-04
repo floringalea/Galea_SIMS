c827179ee0464e0db2a5d245252f99dded423944
Florin Galea <floringalea@gmail.com>
2019-04-04 19:08:57 +0100

    Updated 'back to dashboard' link on error page

--- app/views/error/index.php
+++ app/views/error/index.php
@@ -2,6 +2,6 @@
 
         <h2><?php echo($data['error']); ?></h2>
         <hr>
-        <a href="http://localhost/Galea_SIMS/dashboard/index"> Back to Dashboard</a>
+        <a href="https://www.galeadigital.com/dashboard/index"> Back to Dashboard</a>
 
 <?php include_once('../app/views/templates/footer.php') ?>
