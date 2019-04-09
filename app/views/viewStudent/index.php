d65f60d2cb432d1d8a4b1e8c28c3fbacecce3241
Florin Galea <floringalea@gmail.com>
2019-04-09 22:04:16 +0100

    Added a photo div and some student details to the View Student page

--- app/views/viewStudent/index.php
+++ app/views/viewStudent/index.php
@@ -10,11 +10,27 @@
 </div>
 
 <div class=container-fluid>
-    <div class="col-md-4">
-        <h4>Student Photo</h4>
-    </div>
-    <div class="col-md-8">
-        <h4>Student Details</h4>
-        <p><?php var_dump($data); ?></p>
+    <div class="row">
+        <div class="col-md-2">
+            <h4>Student Photo</h4>
+        </div>
+        <div class="col-md-2">
+            <h4>Forename </h4><p><?php //echo($data['student']->getForename()); ?></p>
+            <h4>Middlename(s)</h4>
+            <h4>Surname </h4>
+            <h4>Date of birth</h4>
+            <h4>UPN </h4>
+            <h4>UCI </h4>
+        </div>
+        <div class="col-md-2">
+            <p><?php echo $data['student']->getForename(); ?></p>
+            <p><?php echo($data['student']->getMiddleName1().", ".$data['student']->getMiddleName2()); ?></p>
+            <p><?php echo($data['student']->getLastName()); ?></p>
+            <p><?php echo($data['student']->getUPN()); ?></p>
+            <p><?php echo($data['student']->getUCI()); ?></p>
+        </div>
+        <div class="col-md-6">
+           
+        </div>
     </div>
 </div>
