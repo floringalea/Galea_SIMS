bb490c4ed463656f4334976e0d2d8d2e15ae0fd7
Florin Galea <floringalea@gmail.com>
2019-04-09 21:56:31 +0100

    Added searchForStudent function to the finder model, to query the database for student records. Accepted search values are forename, surname, UPN or UCI

--- app/models/finder.php
+++ app/models/finder.php
@@ -15,4 +15,21 @@ class Finder extends User
         }
         return $studData;
     }
+	
+	function searchForStudent($criteria)
+	{
+		require "../app/config.php";
+		$result = mysqli_query($conn, "SELECT Title, Forename, LastName, Age, nCyearActual, UPN, UCI, Gender, Student.UserId FROM User, Student WHERE User.UserId = Student.UserId");
+        $studData = [];
+		
+        while($r=mysqli_fetch_assoc($result))
+        {
+            if(strcasecmp($r['Forename'], $criteria) == 0 || $r['LastName'] == $criteria || $r['UPN'] == $criteria || $r['UCI'] == $criteria)
+			{	
+				$studData['0'] = $r;
+				break;
+			}
+        }
+        return $studData;
+	}
 }
