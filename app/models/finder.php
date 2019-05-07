33d64f66dddaa03188492011f6125e876ae67b6e
Florin Galea <floringalea@gmail.com>
2019-05-07 03:46:35 +0100

    Added search for admin users, courses and subjects to finder object

--- app/models/finder.php
+++ app/models/finder.php
@@ -31,5 +31,68 @@ class Finder extends User
 			}
         }
         return $studData;
+    }
+    
+    function searchForAdmin($criteria)
+	{
+		require "../app/config.php";
+		$result = mysqli_query($conn, "SELECT Title, Forename, LastName, Position FROM User, Admin WHERE User.UserId = Admin.UserId");
+        $data = [];
+        while($r=mysqli_fetch_assoc($result))
+        {
+            if(strcasecmp($r['Forename'], $criteria) == 0 || strcasecmp($r['LastName'], $criteria) == 0 || strcasecmp($r['Position'], $criteria) == 0)
+			{	
+                $data['0'] = $r;
+				break;
+            }
+        }
+        return $data;
 	}
+	
+	function getAdminUsersFromDB()
+    {
+        require "../app/config.php";
+        $result = mysqli_query($conn, "SELECT UserId, Position FROM Admin");
+        $i = 0;
+        while($r=mysqli_fetch_assoc($result))
+        {
+            //$adminData[$i] = $r;
+			$secResult = mysqli_query($conn, "SELECT Title, Forename, LastName, Gender FROM User WHERE UserId = ".$r['UserId']);
+			while($z=mysqli_fetch_assoc($secResult))
+			{
+				$adminRecord[$i] = $z;
+				$adminRecord[$i]['position'] = $r['Position'];
+			}
+            $i++;
+        }
+        return $adminRecord;
+    }
+
+    function getCoursesFromDB()
+    {
+        require "../app/config.php";
+        $result = mysqli_query($conn, "SELECT CourseId, Name, TeacherId, SubjectId, Year, StartDate, Room, SessionsTotal FROM Course");
+        $i = 0;
+        $courseData = [];
+        while($r=mysqli_fetch_assoc($result))
+        {
+            $courseData[$i] = $r;
+            $i++;
+        }
+        return $courseData;
+    }
+
+    function getSubjectsFromDB()
+    {
+        require "../app/config.php";
+        $result = mysqli_query($conn, "SELECT SubjectId, SubjectName, Year FROM Subject");
+        $i = 0;
+        $subjData = [];
+        while($r=mysqli_fetch_assoc($result))
+        {
+            $subjData[$i] = $r;
+            $i++;
+        }
+        return $subjData;
+    }
 }
