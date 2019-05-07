79fda3e7c8febb5f68b03e3831f6950b36e8857b
Florin Galea <floringalea@gmail.com>
2019-05-07 03:52:02 +0100

    Removed unused 'Admin' link from 'superadmin' view

--- app/views/superadmin/index.php
+++ app/views/superadmin/index.php
@@ -5,8 +5,7 @@
         <div class="sidebar">
             <a href="https://www.galeadigital.com/staff/index"><i class="fa fa-fw fa-user"></i> Staff</a>
             <a href="https://www.galeadigital.com/students/index"><i class="fa fa-fw fa-child"></i> Students</a>
-            <a href="#Admin"><i class="fa fa-fw fa-wrench"></i> Admin</a>
-            <a href="#Curriculum"><i class="fa fa-fw fa-book"></i> Curriculum</a>
+            <a href="https://www.galeadigital.com/curriculum/index"><i class="fa fa-fw fa-book"></i> Curriculum</a>
             <a href="#Timetables"><i class="fa fa-fw fa-calendar"></i> Timetables</a>
             <a href="https://www.galeadigital.com/userauth/logout"><i class="fa fa-fw fa-lock"></i> Logout</a>
         </div>
