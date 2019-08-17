d0c237b98cf52716ec5260a879dcbe99633f3ea9
Florin Galea <floringalea@gmail.com>
2019-08-17 13:23:57 +0100

    Created 'FormGroup' class

--- app/models/DayCalendar.php
+++
@@ -1,5 +0,0 @@
-<?php
-class DayCalendar
-{
-    function __construct() {}
-}
--- app/views/confLogOut/index.php
+++
@@ -1,7 +0,0 @@
-<?php include_once('../app/views/templates/header.php') ?>
-
-        <h2>You Have Successfully signed out.</h2>
-        <hr>
-        <a href="https://www.galeadigital.com">Back to login</a>
-
-<?php include_once('../app/views/templates/footer.php') ?>
